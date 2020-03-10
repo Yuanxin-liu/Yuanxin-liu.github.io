1ddfe5426e618e4bc6d9d442ac9782be0f6a2587
Yuanxin-liu <18785083168@163.com>
2020-03-10 21:25:13 +0800

    Site updated: 2020-03-10 21:25:10

--- dist/music.js
+++ dist/music.js
@@ -8,9 +8,21 @@ const ap = new APlayer({
 
     audio: [
       {
+        name: "野孩子",
+        artist: '杨千嬅',
+        url: 'http://m10.music.126.net/20200310214554/9805fc32843e7bfe2f71c2b92cdfadac/ymusic/023f/4148/8ce5/29ff2cbc5f95f79f3d9ce6a504ce76e1.mp3',
+      
+      },
+      {
         name: '小半',
         artist: '陈粒',
         url: 'http://m10.music.126.net/20200310192313/e7f138d7d5f10d6870b958a344455767/ymusic/2d15/d9fd/57cd/6679396a63ff496ecef0453b25612dfa.mp3',
+        
+      },
+      {
+        name: '处处吻（Cover：于梓贝）',
+        artist: '邓壬鑫',
+        url: 'http://m10.music.126.net/20200310214805/e4b88acc7782e6f085f1f62c49868bec/ymusic/0f59/0f5a/510c/83f5b655b080b9530169ff1dec2e2624.mp3',
         
       },
       {
