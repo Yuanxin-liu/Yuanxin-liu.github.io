b7acb4549339bc05839ad9f16065455c91c17967
Yuanxin-liu <18785083168@163.com>
2020-03-10 19:31:07 +0800

    Site updated: 2020-03-10 19:31:04

--- dist/music.js
+++ dist/music.js
@@ -7,12 +7,12 @@ const ap = new APlayer({
     volume: 0.7,
 
     audio: [
-      {
-        name: "野孩子",
-        artist: '杨千嬅',
-        url: 'http://m10.music.126.net/20200310191001/3e7889423162dc05d88323000f222a67/ymusic/023f/4148/8ce5/29ff2cbc5f95f79f3d9ce6a504ce76e1.mp3',
+      // {
+      //   name: "野孩子",
+      //   artist: '杨千嬅',
+      //   url: 'http://m10.music.126.net/20200310191001/3e7889423162dc05d88323000f222a67/ymusic/023f/4148/8ce5/29ff2cbc5f95f79f3d9ce6a504ce76e1.mp3',
       
-      },
+      // },
       {
         name: '小半',
         artist: '陈粒',
@@ -22,21 +22,21 @@ const ap = new APlayer({
       {
         name: '画',
         artist: 'G.E.M.邓紫棋',
-        url: 'http://m10.music.126.net/20200310190802/3d1ff26035ac14558221a3e890791405/ymusic/225a/0b65/aa03/90387f0b1f4558c67fe72c77f39e5b62.mp3',
-        
-      },
-      {
-        name: '处处吻（Cover：于梓贝）',
-        artist: '邓壬鑫',
-        url: 'http://m10.music.126.net/20200310191258/291a61fa3a36d7c6e9d7b35b9915f229/ymusic/0f59/0f5a/510c/83f5b655b080b9530169ff1dec2e2624.mp3',
-        
-      },
-      {
-        name: '烟霞',
-        artist: '容祖儿',
-        url: 'http://dl.stream.qqmusic.qq.com/C100002BdYFT0uEEWt.m4a?fromtag=38',
+        url: 'http://music.163.com/#/song?id=412911436',
         
       }
+      // {
+      //   name: '处处吻（Cover：于梓贝）',
+      //   artist: '邓壬鑫',
+      //   url: 'http://m10.music.126.net/20200310191258/291a61fa3a36d7c6e9d7b35b9915f229/ymusic/0f59/0f5a/510c/83f5b655b080b9530169ff1dec2e2624.mp3',
+        
+      // }
+      // {
+      //   name: '烟霞',
+      //   artist: '容祖儿',
+      //   url: 'http://dl.stream.qqmusic.qq.com/C100002BdYFT0uEEWt.m4a?fromtag=38',
+        
+      // }
 
     ]
 });
