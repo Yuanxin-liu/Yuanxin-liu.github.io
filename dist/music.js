231ab8430bac5c59555dd629553c504ce8f4e00f
Yuanxin-liu <18785083168@163.com>
2020-03-10 20:15:09 +0800

    Site updated: 2020-03-10 20:15:07

--- dist/music.js
+++ dist/music.js
@@ -16,7 +16,7 @@ const ap = new APlayer({
       {
         name: '画',
         artist: 'G.E.M.邓紫棋',
-        url: 'http://m10.music.126.net/20200310203500/01ac71fa281cefaa4a6441761303dd09/ymusic/225a/0b65/aa03/90387f0b1f4558c67fe72c77f39e5b62.mp3',
+        url: 'http://m10.music.126.net/20200310203918/31506461a2903b4165bed810d3f8a154/ymusic/225a/0b65/aa03/90387f0b1f4558c67fe72c77f39e5b62.mp3',
         
       }
 
