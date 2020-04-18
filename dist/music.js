26110b61c8038d454822dbd5588e750c55dc86c4
Yuanxin-liu <18785083168@163.com>
2020-04-18 13:33:28 +0800

    Site updated: 2020-04-18 13:32:40

--- dist/music.js
+++ dist/music.js
@@ -8,27 +8,27 @@ const ap = new APlayer({
 
     audio: [
       {
-        name: "野孩子",
-        artist: '杨千嬅',
-        url: 'http://m10.music.126.net/20200310214554/9805fc32843e7bfe2f71c2b92cdfadac/ymusic/023f/4148/8ce5/29ff2cbc5f95f79f3d9ce6a504ce76e1.mp3',
+        name: "I don't wanna see u anymore",
+        artist: 'NINEONE',
+        url: 'http://m8.music.126.net/20200418134246/3a88d160a6fe8a9b6671cc2c0befea11/ymusic/b5b5/9b5d/7ad7/fb41cc3731e57e6cdd5461c56085b3ba.mp3',
       
       },
       {
-        name: '小半',
+        name: '虚拟',
         artist: '陈粒',
-        url: 'http://m10.music.126.net/20200310192313/e7f138d7d5f10d6870b958a344455767/ymusic/2d15/d9fd/57cd/6679396a63ff496ecef0453b25612dfa.mp3',
+        url: 'http://m8.music.126.net/20200418133942/21ebbd05f53a67fdb235a7807ffeaa83/ymusic/db30/6d6f/3dc4/1b2826f685d65de427468badbbfa9390.mp3',
         
       },
       {
         name: '处处吻（Cover：于梓贝）',
         artist: '邓壬鑫',
-        url: 'http://m10.music.126.net/20200310214805/e4b88acc7782e6f085f1f62c49868bec/ymusic/0f59/0f5a/510c/83f5b655b080b9530169ff1dec2e2624.mp3',
+        url: 'http://m8.music.126.net/20200418133553/2bebd55cdfe27fb80490bec4203ec037/ymusic/0f59/0f5a/510c/83f5b655b080b9530169ff1dec2e2624.mp3',
         
       },
       {
-        name: '烟霞',
-        artist: '容祖儿',
-        url: 'https://music.apple.com/cn/album/%E7%83%9F%E9%9C%9E/1211640810?i=1211641243',
+        name: '野孩子',
+        artist: '杨千嬅',
+        url: 'http://m7.music.126.net/20200418134652/282d7fa5c9f686380aa753f56fbaac2b/ymusic/023f/4148/8ce5/29ff2cbc5f95f79f3d9ce6a504ce76e1.mp3',
         
       }
 
