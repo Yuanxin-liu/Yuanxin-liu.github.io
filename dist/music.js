74d490e75f1e21929e7a384bf42fe03780f1380a
Yuanxin-liu <18785083168@163.com>
2020-07-08 12:05:21 +0800

    Site updated: 2020-07-08 12:05:10

--- dist/music.js
+++ dist/music.js
@@ -69,10 +69,10 @@ const ap = new APlayer({
       
       },
       {
-        name: '情人',
-        artist: '蔡徐坤',
-        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%83%85%E4%BA%BA.mp3',
-        cover: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%83%85%E4%BA%BA.jpg',
+        name: '起风了',
+        artist: '吴青峰',
+        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp3',
+        cover: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E8%B5%B7%E9%A3%8E%E4%BA%86.jpg',
         
       },
       {
@@ -96,10 +96,23 @@ const ap = new APlayer({
 
       },
       {
-        name: '野孩子',
-        artist: '杨千嬅',
-        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E9%87%8E%E5%AD%A9%E5%AD%90.mp3',
-        cover: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E9%87%8E%E5%AD%A9%E5%AD%90.jpeg',
+        name: '易燃易爆炸',
+        artist: '陈粒',
+        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%98%93%E7%87%83%E6%98%93%E7%88%86%E7%82%B8.mp3',
+        cover: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%98%93%E7%87%83%E6%98%93%E7%88%86%E7%82%B8.jpeg',
+
+      },
+      {
+        name: '此生不换',
+        artist: '青鸟飞鱼',
+        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%AD%A4%E7%94%9F%E4%B8%8D%E6%8D%A2.mp3',
+        cover: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E6%AD%A4%E7%94%9F%E4%B8%8D%E6%8D%A2.jpg',
+
+      },
+      {
+        name: '小小',
+        artist: '容祖儿',
+        url: 'https://media-yuanxin.oss-cn-hangzhou.aliyuncs.com/music/%E5%B0%8F%E5%B0%8F.mp3',
 
       }
     ]
